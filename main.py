7adb2dd2e52f9be7af036a4b3c185c3b52d5cfb3
Akash Mishra <akash.mishra13@gmail.com>
2022-05-05 02:14:11 -0400

    bugix: Config Loader snipped added to main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import argparse
+
 from Trainer import Trainer
 
 if __name__ == "__main__":
-	Trainer().train()
+	parser = argparse.ArgumentParser(description='Configuration File Path')
+    parser.add_argument(
+        "-c", "--conf", action="store", dest="conf_file",
+        help="Path to config file"
+    )
+	args = parser.parse_args()
+	conf_path =  args.conf_file
+	if conf_path is None:
+		conf_path = 'configs/config.yaml'
+	Trainer(conf_path).train()
