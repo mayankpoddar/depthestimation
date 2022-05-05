ac63f199cc8bb4a57b628db463ad5d274e82ecd3
Mayank Poddar <mayankkpoddar@gmail.com>
2022-05-05 19:52:27 -0400

    Automasking feature correction in Losses

--- Losses/Loss.py
+++ Losses/Loss.py
@@ -5,11 +5,12 @@ from Losses.ReprojectionLoss import ReprojectionLoss
 from Losses.SmoothLoss import SmoothLoss
 
 class Loss(nn.Module):
-    def __init__(self, numScales, frameIdxs, device):
+    def __init__(self, numScales, frameIdxs, device, automasking=False):
         super(Loss, self).__init__()
         self.numScales = numScales
         self.frameIdxs = frameIdxs
         self.device = device
+        self.automasking = automasking
         self.reprojectionLoss = ReprojectionLoss()
         self.smoothLoss = SmoothLoss()
 
@@ -27,18 +28,24 @@ class Loss(nn.Module):
                 pred = outputs[("color", frameIdx, scale)]
                 reprojectionLoss.append(self.reprojectionLoss(pred, target))
             reprojectionLoss = torch.cat(reprojectionLoss, 1)
-            identityReprojectionLoss = []
-            for frameIdx in self.frameIdxs[1:]:
-                pred = inputs[("color", frameIdx, sourceScale)]
-                identityReprojectionLoss.append(self.reprojectionLoss(pred, target))
-            identityReprojectionLoss = torch.cat(identityReprojectionLoss, 1)
-            identityReprojectionLoss += torch.randn(identityReprojectionLoss.shape, device=self.device) * 0.00001
-            combined = torch.cat((identityReprojectionLoss, reprojectionLoss), 1)
+            reprojectionLoss = reprojectionLoss.mean(1, keepdim=True)
+            if self.automasking:
+                identityReprojectionLoss = []
+                for frameIdx in self.frameIdxs[1:]:
+                    pred = inputs[("color", frameIdx, sourceScale)]
+                    identityReprojectionLoss.append(self.reprojectionLoss(pred, target))
+                identityReprojectionLoss = torch.cat(identityReprojectionLoss, 1)
+                identityReprojectionLoss = identityReprojectionLoss.mean(1, keepdim=True)
+                identityReprojectionLoss += torch.randn(identityReprojectionLoss.shape, device=self.device) * 0.00001
+                combined = torch.cat((identityReprojectionLoss, reprojectionLoss), 1)
+            else:
+                combined = reprojectionLoss
             if combined.shape[1] == 1:
                 toOptimise = combined
             else:
                 toOptimise, idxs = torch.min(combined, dim=1)
-            outputs["identity_selection/{}".format(scale)] = (idxs > identityReprojectionLoss.shape[1] - 1).float()
+            if self.automasking:
+                outputs["identity_selection/{}".format(scale)] = (idxs > identityReprojectionLoss.shape[1] - 1).float()
             loss += toOptimise.mean()
             meanDisp = disp.mean(2, True).mean(3, True)
             normDisp = disp / (meanDisp + 1e-7)
